bba2abeafaa425c4978b725d0d9adc7b749e875e
Vaibhav Singh <vaibhavsingh100070@gmail.com>
2020-02-01 11:57:03 +0530

    SignOut Added

--- app/src/main/java/com/example/instagramclone/Profile/SignOutFragment.java
+++ app/src/main/java/com/example/instagramclone/Profile/SignOutFragment.java
@@ -1,25 +1,122 @@
 package com.example.instagramclone.Profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.instagramclone.Login.LoginActivity;
 import com.example.instagramclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignOutFragment extends Fragment {
 
     private static final String TAG = "SignOutFragment";
-    
+
+    //  firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private ProgressBar mProgressBar;
+    private TextView tvSignout;
+    private TextView tvSigningout;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signout , container , false);
-        
+
+        tvSignout = view.findViewById(R.id.tvConfirmSignout);
+        mProgressBar = view.findViewById(R.id.progressBar);
+        tvSigningout = view.findViewById(R.id.tvsignout);
+        Button btnConfirmSignout = view.findViewById(R.id.btnConfirmSignout);
+
+        mProgressBar.setVisibility(View.GONE);
+        tvSigningout.setVisibility(View.GONE);
+
+        setupFirebaseAuth();
+
+        btnConfirmSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to sign out");
+                mProgressBar.setVisibility(View.VISIBLE);
+                tvSigningout.setVisibility(View.VISIBLE);
+
+                mAuth.signOut();
+                Intent intent = new Intent(getActivity() , LoginActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                ActivityCompat.finishAffinity(getActivity());
+                getActivity().finish();
+            }
+        });
+
         return view;
     }
+
+
+    /**
+     * ********************************** firebase ************************************************
+     */
+
+    /**
+     * Setup the firebase auth
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase Auth");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                //  Check if the current user is logged in
+//                checkCurrentUser(user);
+                Log.d(TAG, "onAuthStateChanged: setupFirebaseAuth");
+
+
+                if(user != null){
+                    //  User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in:" + user.getUid());
+                } else{
+                    //  User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out ");
+
+                    Log.d(TAG, "onAuthStateChanged: navigating to login screen");
+                    Intent intent = new Intent(getActivity() , LoginActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };
+    }
+
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+//        checkCurrentUser(mAuth.getCurrentUser());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuth != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    */
 }
---
+++ app/src/main/java/com/example/instagramclone/Utils/FirebaseMethods.java
@@ -0,0 +1,127 @@
+package com.example.instagramclone.Utils;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.instagramclone.Models.User;
+import com.example.instagramclone.Models.UserAccountSettings;
+import com.example.instagramclone.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseMethods {
+    private static final String TAG = "FirebaseMethods";
+
+    private Context mContext;
+
+    //  firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+
+    private String userId;
+    public FirebaseMethods(Context context){
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        mContext = context;
+
+        if(mAuth.getCurrentUser() != null){
+            userId = mAuth.getCurrentUser().getUid();
+        }
+    }
+
+    public boolean checkIfUsernameExists(String username , DataSnapshot dataSnapshot){
+        Log.d(TAG, "checkIfUsernameExists: checking if " + username +" already exists.");
+
+        User user = new User();
+        for(DataSnapshot ds : dataSnapshot.child(userId).getChildren()){
+            Log.d(TAG, "checkIfUsernameExists: datasnapshot " + ds);
+
+            user.setUsername(ds.getValue(User.class).getUsername());
+            Log.d(TAG, "checkIfUsernameExists: username " + user.getUsername());
+
+            if(StringManipulation.expandUsername(user.getUsername()).equals(username)){
+                Log.d(TAG, "checkIfUsernameExists: FOUND A MATCH: " + username);
+                return true;
+            }
+        }
+
+        return  false;
+    }
+
+    /**
+     * Register a new email and password for firebase authentication
+     * @param email
+     * @param password
+     * @param username
+     */
+    public void registerNewEmail(String email,String password,String username){
+        Log.d(TAG, "registerNewEmail: " + mContext);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+
+                            userId = mAuth.getCurrentUser().getUid();
+                            sendVerificationEmail();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(mContext, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void sendVerificationEmail(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            user.sendEmailVerification()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+
+                            } else{
+                                Toast.makeText(mContext , "Couldn't send verification email" , Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    public void addNewUser(String email , String username,String description,String website,String profile_photo){
+        User user = new User(userId , email , StringManipulation.condenseUsername(username) , 1);
+
+        myRef.child(mContext.getString(R.string.dbname_users))
+                .child(userId)
+                .setValue(user);
+
+        UserAccountSettings settings = new UserAccountSettings(
+                description , username , 0 , 0 , 0 , profile_photo , username , website);
+
+        myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                .child(userId)
+                .setValue(settings);
+    }
+}
